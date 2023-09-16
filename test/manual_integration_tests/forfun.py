0ab95f3c70cdce7a39ec521ac3ae658f6cfeac6f
Katchietsnouva <philipaswa01@gmail.com>
2023-09-16 23:01:57 +0300

    Update forfun.py

--- test/manual_integration_tests/forfun.py
+++ test/manual_integration_tests/forfun.py
@@ -1,13 +1,18 @@
-# import tkinter as tk
-# screen = tk.Tk()
-# screen.geometry('300x400')
-# screen.title('Nouva Music')
+# import customtkinter as ctk
+# ctk.set_default_color_theme('dark-blue')
+# screen = ctk.CTk()
+# screen.geometry('350x450')
+# screen.title('Nouvagen')
 # screen.mainloop()
 
-import customtkinter as ctk
 
-ctk.set_default_color_theme('dark-blue')
-screen = ctk.CTk()
-screen.geometry('350x450')
-screen.title('Nouvagen')
-screen.mainloop()
+import tkinter as tk
+from tkinter import ttk
+
+window = ttk.Window(themename='dark')
+window = tk.Tk()
+window.geometry('300x400')
+window.title('Nouva Music')
+title_label = ttk.Label(master=window, font='Calibri', text='Input' )
+title_label.pack()
+window.mainloop()

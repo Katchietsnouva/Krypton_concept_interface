0a09a3a8cdd9cffad2111a48cb6bb94276bd8dc1
Katchietsnouva <philipaswa01@gmail.com>
2023-09-16 21:20:15 +0300

    Update krypton_concept_UI.py

--- test/manual_integration_tests/krypton_concept_UI.py
+++ test/manual_integration_tests/krypton_concept_UI.py
@@ -12,16 +12,14 @@ class App(customtkinter.CTk):
 
         self.button_1 = customtkinter.CTkButton(master=self, text="askopenfile", command=lambda: print(customtkinter.filedialog.askopenfile()))
         self.button_1.pack(pady=10)
-        self.button_2 = customtkinter.CTkButton(master=self, text="askopenfiles", command=lambda: print(customtkinter.filedialog.askopenfiles()))
-        self.button_2.pack(pady=10)
+        
         self.button_3 = customtkinter.CTkButton(master=self, text="askdirectory", command=lambda: print(customtkinter.filedialog.askdirectory()))
         self.button_3.pack(pady=10)
         self.button_4 = customtkinter.CTkButton(master=self, text="asksaveasfile", command=lambda: print(customtkinter.filedialog.asksaveasfile()))
         self.button_4.pack(pady=10)
         self.button_5 = customtkinter.CTkButton(master=self, text="askopenfilename", command=lambda: print(customtkinter.filedialog.askopenfilename()))
         self.button_5.pack(pady=10)
-        self.button_6 = customtkinter.CTkButton(master=self, text="askopenfilenames", command=lambda: print(customtkinter.filedialog.askopenfilenames()))
-        self.button_6.pack(pady=10)
+       
         self.button_7 = customtkinter.CTkButton(master=self, text="asksaveasfilename", command=lambda: print(customtkinter.filedialog.asksaveasfilename()))
         self.button_7.pack(pady=10)
 

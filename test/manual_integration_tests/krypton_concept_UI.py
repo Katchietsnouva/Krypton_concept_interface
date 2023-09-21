cd11a4661b6832296d72e5d7a4e09f4b5b821c5e
Katchietsnouva <philipaswa01@gmail.com>
2023-09-21 20:07:16 +0300

    c
    C

--- test/manual_integration_tests/krypton_concept_UI.py
+++ test/manual_integration_tests/krypton_concept_UI.py
@@ -66,9 +66,11 @@ def open_file_name_dialog():
 def save_as_file_name_dialog():
     print(customtkinter.filedialog.asksaveasfilename())
 
-button_open_file = customtkinter.CTkButton(app, text="Open File Dialog", command=open_file_dialog)
-button_open_directory = customtkinter.CTkButton(app, text="Open Directory Dialog", command=open_directory_dialog)
-button_save_as_file = customtkinter.CTkButton(app, text="Save As File Dialog", command=save_as_file_dialog)
+openfile_frame = ttk.Frame(master=app)
+
+button_open_file = customtkinter.CTkButton(openfile_frame, text="Open File Dialog", command=open_file_dialog)
+button_open_directory = customtkinter.CTkButton(openfile_frame, text="Open Directory Dialog", command=open_directory_dialog)
+button_save_as_file = customtkinter.CTkButton(openfile_frame, text="Save As File Dialog", command=save_as_file_dialog)
 button_open_file_name = customtkinter.CTkButton(app, text="Open File Name Dialog", command=open_file_name_dialog)
 button_save_as_file_name = customtkinter.CTkButton(app, text="Save As File Name Dialog", command=save_as_file_name_dialog)
 

d76bc6ebbfe710f5eba9ad50e0e8b356e6667e2d
Katchietsnouva <philipaswa01@gmail.com>
2023-09-21 23:02:36 +0300

    d
    d

--- test/manual_integration_tests/krypton_concept_UI.py
+++ test/manual_integration_tests/krypton_concept_UI.py
@@ -81,6 +81,16 @@ options_frame.pack()
 combobox_1 = customtkinter.CTkComboBox(app, variable=variable, values=countries, command=select_callback, width=300)
 combobox_1.pack(pady=20, padx=10)
 
+# Create scrollable frame
+scrollable_frame = customtkinter.CTkScrollableFrame(app, label_text="CTkScrollableFrame")
+scrollable_frame.pack(fill="both", expand=True)
+scrollable_frame.grid_columnconfigure(0, weight=1)
+scrollable_frame_switches = []
+for i in range(100):
+    switch = customtkinter.CTkSwitch(master=scrollable_frame, text=f"CTkSwitch {i}")
+    switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+    scrollable_frame_switches.append(switch)
+
 scaling_slider = customtkinter.CTkSlider(app, command=set_new_scaling, from_=0, to=2)
 scaling_slider.pack(pady=20, padx=10)
 
@@ -88,23 +98,21 @@ openfile_frame = ttk.Frame(master=app)
 openfile_frame_sub1 = ttk.Frame(master=openfile_frame)
 
 button_open_file = customtkinter.CTkButton(openfile_frame_sub1, text="Open File Dialog", command=open_file_dialog)
-button_open_file_name = customtkinter.CTkButton(openfile_frame_sub1, text="Open File Name Dialog", command=open_file_name_dialog)
 button_open_directory = customtkinter.CTkButton(openfile_frame_sub1, text="Open Directory Dialog", command=open_directory_dialog)
 
 openfile_frame_sub2 = ttk.Frame(master=openfile_frame)
 
-button_save_as_file = customtkinter.CTkButton(openfile_frame_sub2, text="Save As File Dialog", command=save_as_file_dialog)
-button_save_as_file_name = customtkinter.CTkButton(openfile_frame_sub2, text="Save As File Name Dialog", command=save_as_file_name_dialog)
+button_save_as_file = customtkinter.CTkButton(openfile_frame_sub2, text="Save File As", command=save_as_file_dialog)
+button_save_as_file_name = customtkinter.CTkButton(openfile_frame_sub2, text="Save File Name As", command=save_as_file_name_dialog)
 
-button_open_file.pack(pady=10, padx=10)
-button_open_directory.pack(pady=10, padx=10)
+button_open_file.pack(pady=2, padx=10)
+button_open_directory.pack(pady=2, padx=10)
 
-button_save_as_file.pack(pady=10, padx=10)
-button_save_as_file_name.pack(pady=10, padx=10)
+button_save_as_file.pack(pady=2, padx=10)
+button_save_as_file_name.pack(pady=2, padx=10)
 openfile_frame_sub1.pack(side='left', pady=10, padx=10)
 openfile_frame_sub2.pack(side='left', pady=10, padx=10)
 openfile_frame.pack()
-
 
 
 entry_3 = customtkinter.CTkEntry(app, placeholder_text="placeholder")
@@ -117,15 +125,6 @@ entry_4.pack(pady=(20, 20))
 entry_4.insert(0, "sdfjk")
 entry_4.delete(0, 2)
 
-# Create scrollable frame
-scrollable_frame = customtkinter.CTkScrollableFrame(app, label_text="CTkScrollableFrame")
-scrollable_frame.pack(fill="both", expand=True)
-scrollable_frame.grid_columnconfigure(0, weight=1)
-scrollable_frame_switches = []
-for i in range(100):
-    switch = customtkinter.CTkSwitch(master=scrollable_frame, text=f"CTkSwitch {i}")
-    switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-    scrollable_frame_switches.append(switch)
 
 # Start the main loop
 app.mainloop()
